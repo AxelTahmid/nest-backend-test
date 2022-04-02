79957f3691c7a521e43929af45b9adbda625846f
AxelTahmid <axel.tahmid@gmail.com>
2022-04-03 04:02:46 +0600

    module export configured, user create and get working

--- src/app.module.ts
+++ src/app.module.ts
@@ -1,15 +1,14 @@
-//src/app.module.ts
-
 import { Module } from '@nestjs/common';
 import { AppController } from './app.controller';
-import { PrismaService } from './prisma.service';
-import { UsersModule } from './users/users.module';
+import { AppService } from './app.service';
+import { PrismaService } from 'src/prisma.service';
 import { UsersService } from './users/users.service';
+import { UsersModule } from './users/users.module';
 import { PasswordProvider } from './providers/password';
 
 @Module({
   imports: [UsersModule],
   controllers: [AppController],
-  providers: [PrismaService, UsersService, PasswordProvider],
+  providers: [AppService, UsersService, PrismaService, PasswordProvider],
 })
 export class AppModule {}
